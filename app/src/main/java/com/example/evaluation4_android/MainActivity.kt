9e65fff89155acf80740f65dc12ab5e85c08bbb9
Abdul Rahman Zahid <103886116+LLAbdul@users.noreply.github.com>
2025-05-10 16:06:56 -0400

    Ajout de la librarie Room avec logique de tache

--- app/src/main/java/com/example/evaluation4_android/MainActivity.kt
+++ app/src/main/java/com/example/evaluation4_android/MainActivity.kt
@@ -37,11 +37,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Evaluation4_AndroidTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/evaluation4_android/data/model/Tache.kt
+++ app/src/main/java/com/example/evaluation4_android/data/model/Tache.kt
@@ -1,2 +1,22 @@
 package com.example.evaluation4_android.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+enum class Priorite {
+    ELEVE,
+    MOYEN,
+    BAS
+}
+@Entity(tableName = "tache")
+data class Tache(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val dateCreation: String,
+    val priorite: Priorite,
+    val nom: String,
+    val note: String?,
+    val isCompleted : Boolean,
+    val expectedDueDate: String,
+
+)
